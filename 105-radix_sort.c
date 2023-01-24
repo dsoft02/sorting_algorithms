e82ee6565514a9ddcbb648543dd037cd5848adc7
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-01-24 11:24:16 -0800

    radix sort

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,92 +1,88 @@
 #include "sort.h"
 
 /**
- * int_array_init - initializes an int array of size size filled with 0
- *
- * @size: the wanted size of the array
- *
- * Return: the new filled array, or NULL on failure
- */
-
-int *int_array_init(size_t size)
+* getMaxDigit - Search the max of an array
+*
+* @array: The given array
+* @size: The size
+*
+* Return: The max
+*/
+int getMaxDigit(int *array, size_t size)
 {
-	int *new = NULL;
 	size_t i;
-
-	new = malloc(sizeof(int) * size);
-	if (new)
-	{
-		for (i = 0; i < size; i++)
-			new[i] = 0;
-	}
-
-	return (new);
-}
-
-/**
- * count_sort - sorts an array of integers using counting sort
- *
- * @array: the array of integers
- * @size: the size of the array
- * @exp: the exponent value of 10
- */
-
-void count_sort(int *array, size_t size, int exp)
-{
-	int *count = NULL, *output = NULL;
-	size_t i;
-
-	count = int_array_init(10 + 1);
-	if (count)
-	{
-		/* Histogram of the number of times a key occurs within the input */
-		for (i = 0; i < size; i++)
-			count[(array[i] / exp) % 10] += 1;
-
-		/* Prefix sum computation on count to determine the position range */
-		for (i = 1; i <= 10; i++)
-			count[i] += count[i - 1];
-
-		output = int_array_init(size);
-		if (output)
-		{
-			/* Moving each item into its sorted position in the output array */
-			for (i = size - 1; (int)i >= 0; i--)
-			{
-				output[count[(array[i] / exp) % 10] - 1] = array[i];
-				count[(array[i] / exp) % 10] -= 1;
-			}
-
-			for (i = 0; i < size; i++)
-				array[i] = output[i];
-
-			free(output);
-		}
-		free(count);
-	}
-}
-
-/**
- * radix_sort - sorts an array of integers using LSD radix sort
- *
- * @array: the array of integers
- * @size: the size of the array
- */
-
-void radix_sort(int *array, size_t size)
-{
-	int k = -1, exp;
-	size_t i;
-
-	if (size < 2)
-		return;
+	int max = 0;
 
 	for (i = 0; i < size; i++)
-		k = array[i] > k ? array[i] : k;
-
-	for (exp = 1; k / exp > 0; exp *= 10)
 	{
-		count_sort(array, size, exp);
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
+
+/**
+* radixSort - Sort the array by the digit
+*
+* @array: Array to be sorted
+* @buf: Buffer of the array, to make opération in before
+*       put all the content back into the array parent
+* @size: Size of the array
+* @digit: The digit to look
+*
+* Return: Anything, cause void function
+*/
+void radixSort(int *array, int *buf, size_t size, size_t digit)
+{
+	size_t i;
+	int arrayCount[10];
+
+	for (i = 0; i < 10; i++)
+		arrayCount[i] = 0;
+
+	for (i = 0; i < size; i++)
+		arrayCount[(array[i] / digit) % 10]++;
+
+	for (i = 1; i < 10; i++)
+		arrayCount[i] += arrayCount[i - 1];
+
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		buf[arrayCount[(array[i] / digit) % 10] - 1] = array[i];
+		arrayCount[(array[i] / digit) % 10] -= 1;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = buf[i];
+}
+
+/**
+* radix_sort - Sort an array using the radix sort
+* methode
+*
+* @array: The array to sort
+* @size: The size of the array
+*
+* Return: Anything, cause void function
+*/
+void radix_sort(int *array, size_t size)
+{
+	size_t max, i;
+	int *buf;
+
+	if (!array || size <= 1)
+		return;
+
+	max = getMaxDigit(array, size);
+
+	buf = malloc(sizeof(int) * size);
+	if (!buf)
+		return;
+
+	for (i = 1; max / i > 0; i *= 10)
+	{
+		radixSort(array, buf, size, i);
 		print_array(array, size);
 	}
+	free(buf);
 }
